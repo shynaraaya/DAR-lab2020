1c4147c22ba258a7d0540174e00f48aba4dfb84a
Ayanbek Shynara <39334586+shynaraaya@users.noreply.github.com>
2020-09-01 17:40:44 +0600

    fix(): finished PUT/:id request

--- myapp/routes/users.js
+++ myapp/routes/users.js
@@ -1,39 +1,11 @@
 var express = require('express');
 var axios = require('axios');
+var fetch = require('node-fetch');
 
 var router = express.Router();
 
-
-
-router.get('/', (req, res, next) => {
-  axios.get(`https://jsonplaceholder.typicode.com/users`)
-    .then(function(response) {
-      res.json(response.data)
-    }).catch(function(error) {
-      res.json('ERROR!!!')
-    })
-});
-
-router.get('/:id', (req, res, next) => {
-  const userId = req.params.id;
-  if (!userId) {
-    res.json("ID is not found!")
-  } else {
-    axios.get(`https://jsonplaceholder.typicode.com/users/${userId}`)
-      .then(function(response) {
-        res.json(response.data)
-      }).catch(function(error) {
-        res.json("ERROR!!!")
-      })
-  }
-});
-
-
-router.post('/add', (req, res, next) => {
-  if(!req.body) 
-    return res.sendStatus(400);
-  axios.post(`https://jsonplaceholder.typicode.com/users`, {
-    name: "Shynara",
+let body = {
+  name: "Shynara",
     username: "shynaraaya",
     email: "aya@gmail.com",
     address: {
@@ -53,12 +25,58 @@ router.post('/add', (req, res, next) => {
       catchPhrase: "Multi-layered client-server neural-net",
       bs: "harness real-time e-markets"
     }
-}).then(function(response) {
-  console.log(response.data)
+}
+
+
+router.get('/', (req, res, next) => {
+  axios.get(`https://jsonplaceholder.typicode.com/users`)
+    .then(function(response) {
+      res.json(response.data)
+    }).catch(function(error) {
+      res.json('ERROR!!!')
+    })
+});
+
+router.get('/:id', (req, res, next) => {
+  let userId = req.params.id;
+  if (!userId) {
+    res.json("ID is not found!")
+  } else {
+    axios.get(`https://jsonplaceholder.typicode.com/users/${userId}`)
+      .then(function(response) {
+        res.json(response.data)
+      }).catch(function(error) {
+        res.json("ERROR!!!")
+      })
+  }
+});
+
+router.post('/post', (req, res, next) => {
+  if(!req.body)
+    return res.sendStatus(400);
+  //this case without ready data
+  //axios.post(`https://jsonplaceholder.typicode.com/users`, req.body).
+  //I used my own example to check how it works through postman
+  axios.post(`https://jsonplaceholder.typicode.com/users`, body).
+  then(function(response) {
+  res.json(response.data)
 }).catch(function(error) {
   res.json('ERROR!!!')
 })
 });
+
+router.put('/:id', (req, res, next) => {
+  let userId = req.params.id;
+  if(!req.body)
+    return res.sendStatus(400);
+  axios.put(`https://jsonplaceholder.typicode.com/users/${userId}`, body)
+  .then(function(response) {
+  res.json(response.data)
+}).catch(function(error) {
+  res.json('ERROR!!!')
+})
+});
+
 
 
 module.exports = router;
